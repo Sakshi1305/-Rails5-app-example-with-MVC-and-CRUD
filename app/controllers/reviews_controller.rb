1b9bad743ee5aa2591aaf019956aea69d7f759a4
Sakshi1305 <sakshijain1305@gmail.com>
2018-10-08 10:46:45 +0530

    Add model validation.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,22 @@
 class ReviewsController < ApplicationController
   def create
     @film = Film.find_by(id: params[:id])
-    if @film.reviews.create!(permitted_review_params)
-      redirect_to @film
-    else
-      render 'new'
+    @reviews = @film.reviews.new(permitted_review_params)
+    respond_to do |format|
+      if @reviews.save
+        format.js{redirect_to @film}
+      else
+        format.js
+        @reviews.errors.any?
+        @reviews.errors.each do |key, value|
+        end
+      end
     end
+    # if @film.reviews.create!(permitted_review_params)
+    #   redirect_to @film
+    # else
+    #   render 'new'
+    # end
   end
 
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :film
+  validates :email, presence: :true
+  validates_format_of :email, :with=> /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create, message: "Please enter a valid email address."
 end
